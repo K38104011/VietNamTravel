6174950d36254bc9cbc169276b237cfc274e50c0
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-09-09 12:42:55 +0700

    add web api

--- VNT.Core/VNT.UI.Web/Global.asax.cs
+++ VNT.Core/VNT.UI.Web/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using VNT.UI.Web.App_Start;
 
 namespace VNT.UI.Web
 {
@@ -14,6 +12,7 @@ namespace VNT.UI.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(ApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
---
+++ VNT.Core/VNT.UI.Web/WebApi/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace VNT.UI.Web.WebApi
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public string GetData()
+        {
+            return "Hello";
+        }
+    }
+}
