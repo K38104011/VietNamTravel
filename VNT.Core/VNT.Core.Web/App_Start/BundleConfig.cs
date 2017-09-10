c1bfe6df400758f45b98c225c3a3ab2e76671e7f
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-09-10 13:19:31 +0700

    - add dash gum bootstrap template

--- VNT.Core/VNT.Core.Web/App_Start/BundleConfig.cs
+++ VNT.Core/VNT.Core.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,29 @@ namespace VNT.UI.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/themeScript").Include(
+                "~/Content/assets/js/jquery.js",
+                "~/Content/assets/js/jquery-1.8.3.min.js",
+                "~/Content/assets/js/bootstrap.min.js",
+                "~/Content/assets/js/jquery.dcjqaccordion.2.7.js",
+                "~/Content/assets/js/jquery.scrollTo.min.js",
+                "~/Content/assets/js/jquery.nicescroll.js",
+                "~/Content/assets/js/jquery.sparkline.js",
+                "~/Content/assets/js/common-scripts.js",
+                "~/Content/assets/js/gritter/js/jquery.gritter.js",
+                "~/Content/assets/js/gritter-conf.js",
+                "~/Content/assets/js/chart-master/Chart.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/themeStyle").Include(
+                "~/Content/assets/css/bootstrap.css",
+                "~/Content/assets/font-awesome/css/font-awesome.css",
+                "~/Content/assets/css/zabuto_calendar.css",
+                "~/Content/assets/js/gritter/css/jquery.gritter.css",
+                "~/Content/assets/lineicons/style.css",
+                "~/Content/assets/css/style.css",
+                "~/Content/assets/css/style-responsive.css"
+                ));
         }
     }
 }
